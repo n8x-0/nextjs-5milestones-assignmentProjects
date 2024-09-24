5badddd4d65652195111326a1bac0942a4501247
shayan <syedshayan1324@gmail.com>
2024-09-25 00:14:45 +0500

    first commit

---
+++ milestone1/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-screen bg-blue-500 flex justify-center items-center">
+          <h1 className="text-white font-medium text-5xl tracking-tighter animate-bounce">Hello World</h1>
+      </div>
+    </>
+  );
+}
