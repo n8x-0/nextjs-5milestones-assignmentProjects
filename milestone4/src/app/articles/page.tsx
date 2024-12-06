a49891160be5420a68e79315332cf13a09790ed1
shayan <syedshayan1324@gmail.com>
2024-12-06 14:22:52 +0500

    opt cache behaviour in articles page

--- milestone4/src/app/articles/page.tsx
+++ milestone4/src/app/articles/page.tsx
@@ -7,7 +7,7 @@ const Articles = async () => {
 
   const getBlogs = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/allposts`, {
-      next: { revalidate: 5 }
+      cache: "no-store"
     });
     return await res.json()
   }
